8bf0c1309b649fe14c7467d685f8b2773c0193b6
Aviv Ben Shahar <avivbens87@gmail.com>
2021-09-07 17:38:20 +0300

    .

---
+++ dist/precache-manifest.c3aa4d2cbafdfb0c6873eb5f32f904e8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c553849c3fc38a97b0",
+    "url": "/loop-machine-moveo/css/app.2e67cc02.css"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/loop-machine-moveo/fonts/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/loop-machine-moveo/fonts/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "c91aa73d44ef196450bc5ed71b97332a",
+    "url": "/loop-machine-moveo/img/homephoto.c91aa73d.webp"
+  },
+  {
+    "revision": "0241af7fa3c3e139878f43c4cc289364",
+    "url": "/loop-machine-moveo/img/logo.0241af7f.png"
+  },
+  {
+    "revision": "2b6952d652f66993b16d24a7217dcd3a",
+    "url": "/loop-machine-moveo/index.html"
+  },
+  {
+    "revision": "b5c553849c3fc38a97b0",
+    "url": "/loop-machine-moveo/js/app.7e0a750b.js"
+  },
+  {
+    "revision": "79ee7b50214d493ae06e",
+    "url": "/loop-machine-moveo/js/chunk-vendors.67f9ba90.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/loop-machine-moveo/manifest.json"
+  },
+  {
+    "revision": "ef4b0d6b1224b47afc651b5133353762",
+    "url": "/loop-machine-moveo/media/120_future_funk_beats_25.ef4b0d6b.mp3"
+  },
+  {
+    "revision": "8c298669280c7833651b2456ecc917fa",
+    "url": "/loop-machine-moveo/media/120_stutter_breakbeats_16.8c298669.mp3"
+  },
+  {
+    "revision": "7eea86a441935e6731055bc0dab4644e",
+    "url": "/loop-machine-moveo/media/Bass_Warwick_heavy_funk_groove_on_E_120_BPM.7eea86a4.mp3"
+  },
+  {
+    "revision": "755915305ccf87394b35fc78f9a85058",
+    "url": "/loop-machine-moveo/media/FUD_120_StompySlosh.75591530.mp3"
+  },
+  {
+    "revision": "3b9bb8265a45c63eca769bceb2b4bc9f",
+    "url": "/loop-machine-moveo/media/GrooveB_120bpm_Tanggu.3b9bb826.mp3"
+  },
+  {
+    "revision": "feb935027c75c0d17bf7f28462a5ebe8",
+    "url": "/loop-machine-moveo/media/MazePolitics_120_Perc.feb93502.mp3"
+  },
+  {
+    "revision": "4a48ae8ca3c63791f7d7c8d56a7fc722",
+    "url": "/loop-machine-moveo/media/PAS3GROOVE1.03B.4a48ae8c.mp3"
+  },
+  {
+    "revision": "d1e8bea8f68f0347bad0821cb6855756",
+    "url": "/loop-machine-moveo/media/SilentStar_120_Em_OrganSynth.d1e8bea8.mp3"
+  },
+  {
+    "revision": "14c5bdf8a2f8f968fa56b409bb8908a1",
+    "url": "/loop-machine-moveo/media/electric_guitar_coutry_slide_120bpm-B.14c5bdf8.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/loop-machine-moveo/robots.txt"
+  }
+]);
